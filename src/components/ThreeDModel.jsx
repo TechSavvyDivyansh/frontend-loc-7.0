ca60ba759e595a5c629bbf270b73c68599f8c30a
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2025-02-07 20:19:52 +0530

    changes

--- src/components/ThreeDModel.jsx
+++ src/components/ThreeDModel.jsx
@@ -2,10 +2,11 @@ import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
-    <div className="">
-         <Spline
+    <div className="relative">
+    <div className="z-10"><Spline
         scene="https://prod.spline.design/DQwoPNADmKJ50Z1U/scene.splinecode" 
-      />
+      /></div>
+      <div className="absolute w-[300px] h-[50px] bg-[#0a0a0a] bottom-3 right-0 z-20"></div>
       </div>
   );
 }
